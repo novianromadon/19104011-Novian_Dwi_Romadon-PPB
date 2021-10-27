82d3c096a5d8e189527177fc5801097b6c91697f
Novian Dwi Romadon <72422164+novianromadon@users.noreply.github.com>
2021-10-27 18:50:33 +0700

    Add files via upload

---
+++ Praktikum/Pertemuan 1/Lat1.kt
@@ -0,0 +1,25 @@
+// variabel static
+val valString = "ini String"
+val valInt = 123
+
+// variabel dinamis
+var varString = "iki String"
+var varInt = 456
+
+//Tipe Data
+//secara manual
+var varMString : String = "sebuah teks"
+
+//tipedata referensi
+//var kelas : Kelas
+
+var penggabunganStr = "Angka ${valInt + varInt}"
+println(penggabunganStr)
+
+var gabungStr = "Angka $varInt"
+println(gabungStr)
+
+varString = "ubah"
+
+print("Hello world!")
+println("Hello world!")
---
+++ Praktikum/Pertemuan 1/StateFor.kt
@@ -0,0 +1,10 @@
+fun StateFor () {
+    val ranges = 1.rangeTo(10) step 3
+    
+    //i => variabel yang dipake di dalam loop
+    // ranges => array atau jumlah batasnya
+  
+    for (i in ranges ){
+      println("value is $i!")
+    }
+  }
