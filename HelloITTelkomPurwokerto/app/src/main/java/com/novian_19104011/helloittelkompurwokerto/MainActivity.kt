c0dbd901e2762515f03a477e2b21020b77bec17e
Novian Dwi Romadon <72422164+novianromadon@users.noreply.github.com>
2021-11-03 19:48:58 +0700

    Add files via upload

---
+++ HelloITTelkomPurwokerto/app/src/main/java/com/novian_19104011/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.novian_19104011.helloittelkompurwokerto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPage = findViewById<Button>(R.id.btn_page)
+        btnPage.setOnClickListener {
+            val intent = Intent(this, Halaman2Activity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    fun printState(msg : String) {
+        Log.d("ActivityState", msg) // Logcat, muncul di Logcat Android Studio
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show() // Toast, muncul di UI APP
+    }
+
+    override fun onStart() {
+        super.onStart()
+        printState("Halaman satu onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        printState("Halaman satu onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        printState("Halaman satu onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        printState("Halaman satu onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        printState("Halaman satu onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        printState("Halaman satu onDestroy")
+    }
+
+
+}
